43d31865cea187d929754ad2ffc0aec47cf39d6b
longhowlam <longhowlam@gmail.com>
2017-12-02 20:53:13 +0000

    image dir as paramter namespace dplyr

--- R/scraper.R
+++ R/scraper.R
@@ -1,5 +1,5 @@
 
-searchIkea = function(query){
+searchIkea = function(query, imagedir = "images"){
 
   #### first retrieve N pages on the first page
   baselink = paste0(
@@ -9,66 +9,66 @@ searchIkea = function(query){
   )
   firstpagelink = paste0(baselink , "1")
 
-  flog.info("SEARCH %s", firstpagelink)
-  out = read_html(firstpagelink)
-  tmp =  html_nodes(out, xpath = '//a/@href') %>% html_text()
+  futile.logger::flog.info("SEARCH %s", firstpagelink)
+  out = xml2::read_html(firstpagelink)
+  tmp =  rvest::html_nodes(out, xpath = '//a/@href') %>% rvest::html_text()
   pages = tmp[stringr::str_detect(tmp, "pageNumber")]
 
-  NPAGES = pages %>% str_extract("\\d+") %>% as.numeric() %>% max()
+  NPAGES = pages %>% stringr::str_extract("\\d+") %>% as.numeric() %>% max()
 
-  flog.info("npages %s", NPAGES)
+  futile.logger::flog.info("npages %s", NPAGES)
 
-  flog.info("loop over pages")
+  futile.logger::flog.info("loop over pages")
   #### loop over the search pages
   if(NPAGES > 0){
     iter = 1:NPAGES
-    purrr::map_df(iter, searchIkeaOnePage, baselink, query)
+    purrr::map_df(iter, searchIkeaOnePage, baselink, query, imagedir)
   }
 }
 
 
-searchIkeaOnePage = function(iter, baselink, query)
+searchIkeaOnePage = function(iter, baselink, query, imagedir)
 {
   link = paste0(baselink,iter)
-  out = read_html(link)
+  out = xml2::read_html(link)
 
-  lijst =  html_nodes(out, xpath = '//a/@href') %>% html_text()
-  producten = str_detect(lijst , "catalog/products")
-  bvtab = str_detect(lijst , "bvtab")
+  lijst =  rvest::html_nodes(out, xpath = '//a/@href') %>% rvest::html_text()
+  producten = stringr::str_detect(lijst , "catalog/products")
+  bvtab = stringr::str_detect(lijst , "bvtab")
   geldigeproductlinks = lijst[producten & !bvtab]
 
   outframe = data.frame()
   for( j in 1:length(geldigeproductlinks))
   {
     link = paste0("http://www.ikea.com/", geldigeproductlinks[j])
-    out = read_html(link)
+    out = xml2::read_html(link)
 
     ## retrieve image
-    img = html_nodes(
+    img = rvest::html_nodes(
       out,
       xpath = '//img[@id="productImg"]/@src'
     ) %>%
-      html_text()
+      rvest::html_text()
 
-    imagefile = str_sub(img,24,str_length(img))
+    imagefile = stringr::str_sub(img,24, stringr::str_length(img))
 
     tryCatch(
       download.file(
         paste0("http://www.ikea.com", img),
-        destfile = paste0("images/", imagefile),
+        destfile = paste0(imagedir, "/", imagefile),
         quiet = TRUE
       ),
-      error=function(e)  flog.error("No such file")
+      error=function(e)  futile.logger::flog.error("No such file")
     )
 
     ## retrive breadCrumbs
-    breadCrumbs = html_nodes(
+    breadCrumbs = rvest::html_nodes(
       out,
       xpath = '//ul[@id="breadCrumbs"]'
     ) %>%
-      html_text() %>%
+      rvest::html_text() %>%
       cleanstring() %>%
-      str_split("/")
+      stringr::str_split("/")
     ## Neem 4e breadcrumb als ie niet leeg is anders 3e
     type4 = ifelse(is.na(breadCrumbs[[1]][4]), breadCrumbs[[1]][3], breadCrumbs[[1]][4])
     type3 = ifelse(is.na(breadCrumbs[[1]][3]), breadCrumbs[[1]][2], breadCrumbs[[1]][3])
@@ -78,7 +78,7 @@ searchIkeaOnePage = function(iter, baselink, query)
       data.frame(link,type3, type4,imagefile, stringsAsFactors = FALSE)
     )
   }
-  flog.info("search page %s processed", iter)
+  futile.logger::flog.info("search page %s processed", iter)
   outframe
 }
 
